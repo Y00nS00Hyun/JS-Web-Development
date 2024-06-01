c8f27e81fc59d6ae9e5ce9ff153140632bf23442
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-01 23:34:12 +0900

    body: JSPN.stringify(neeew) const members = JSON.parse(result)

---
+++ WEB-API/JSON 실습.js
@@ -0,0 +1,25 @@
+//자바스크립트 객체 -> JSON (string 타입) -> body: JSON.stringify(newMember),
+//JSON -> 자바스크립트 객체 -> const members = JSON.parse(result);
+
+//1 직원 정보 입력
+const newMember = {
+  name: "Soohyun",
+  email: "abc@naver.com",
+  department: "kitty",
+};
+
+//2 JS -> JSON
+fetch("https://learn.codeit.kr/api/members", {
+  method: "POST",
+  body: JSON.stringify(newMember),
+});
+
+//3 JSON -> JS
+.then(() => {
+    fetch('https://learn.codeit.kr/api/members')
+    .then((response)=>response.text())
+    .then((result)=>{
+        const members = JSON.parse(result);
+        console.log(members[members.length-1]);s
+    })
+});
