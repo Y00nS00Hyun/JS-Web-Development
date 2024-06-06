7b1feb118bde10f88cd733422fd4234a2468ca60
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-06 21:22:32 +0900

    resolve 함수

--- Promise/점심 메뉴 고르기.js
+++ Promise/점심 메뉴 고르기.js
@@ -8,6 +8,7 @@ function pick(menus) {
                 const randomIdx = Math.floor(Math.random() * menus.length);
                 const selectedMenu = menus[randomIdx];
                 resolve(selectedMenu); // 선택된 메뉴를 resolve로 반환
+                //resolve 함수: Promise 객체를 직접 만들 때, 생성된 Promise 객체를 fulfilled 상태로 만들어주는 함수
             }, 1000); // 시간이 걸리는 걸 시뮬레이션하기 위한 1초입니다
         }
     });
