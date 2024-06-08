b7b923f8882e9036f01f5ab6e09fa89be4a77706
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-08 17:50:31 +0900

    같은 결과

--- async/실습.js
+++ async/실습.js
@@ -1,17 +1,17 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => {
-        const lastUser = users[users.length - 1];
-        return lastUser.id;
-    })
-    .then((id) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))
-    .then((response) => response.json())
-    .then((posts) => {
-        const lastPost = posts[posts.length - 1];
-        console.log(lastPost);
-    });
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((users) => {
+//         const lastUser = users[users.length - 1];
+//         return lastUser.id;
+//     })
+//     .then((id) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))
+//     .then((response) => response.json())
+//     .then((posts) => {
+//         const lastPost = posts[posts.length - 1];
+//         console.log(lastPost);
+//     });
 
-//위 함수를 async 로 바꾸자
+// //위 함수를 async 로 바꾸자
 
 
 async function getTheLastPostOfTheLastUser() {
@@ -23,10 +23,12 @@ async function getTheLastPostOfTheLastUser() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const posts = await response.json()
     const lastPost = posts[posts.length - 1];
+    console.log(lastPost);
     return lastPost;
 }
 
-getTheLastPostOfTheLastUser().then((lastPost) => {
-    console.log(lastPost);
-});
+// getTheLastPostOfTheLastUser().then((lastPost) => {
+//     console.log(lastPost);
+// });
+getTheLastPostOfTheLastUser()
 
